6802367f10ff939a090677ac77436ccbfa8006f1
sachinmukherjee <sachinmukherjee29@gmail.com>
2017-07-10 18:44:27 +0530

    Logistic regression implemented from scratch

--- Logistic Regression/logisticregression.py
+++ Logistic Regression/logisticregression.py
@@ -1,36 +1,33 @@
-# a python program for implementing logistic regression
-
-from numpy import *
-
-
-def loadDataSet():
-    dataMat = []
-    labelMat = []
-    fr = open('testSet.txt')
-    for line in fr.readlines():
-        lineArr = line.strip().split()
-        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
-        labelMat.append(int(lineArr[2]))
-    return dataMat, labelMat
+# implementation of logistic regression
+# predicting the output variable from input
+# created by sachin mukherjee
+# sachinmukherjee29@gmail.com
+from math import e
 
 
-def sigmoid(inX):
-    return 1.0 / (1 + exp(-inX))
+def logisticRegression(inp):
+    x1 = inp[0]                                             # x1 from input
+    x2 = inp[1]                                             # x2 from input
+    b0 = 0                                                  # initialising coficient
+    b1 = 0
+    b2 = 0
+    output = 0                                              # initialising output
+    alpha = 0.3                                             # taking alpha as 0.3
+    output = b0 + b1 * x1 + b2 * x2
+    prediction = 1 / (1 + pow(e, -output))
+    for x in range(11):                                     # making 10 iteration for better results
+        # cofficient = previous cofficient value + alpha * (output-prediction) * prediction * (1 - prediction) * input value according to cofficient
+        b0 = b0 + alpha * (output-prediction) * prediction * (1 - prediction) * 1.0
+        b1 = b1 + alpha * (output-prediction) * prediction * (1 - prediction) * x1
+        b2 = b2 + alpha * (output-prediction) * prediction * (1 - prediction) * x2
+        output = b0 + b1 * x1 + b2 * x2                     # calculating output from cofficients
+        prediction = 1 / (1 + pow(e, -output))              # predicting output
+    if prediction > 0.5:
+        print "Class 1"
+    else:
+        print "Class 0"
+
+inp = [2.7810836, 2.550537003]
+logisticRegression(inp)
 
 
-def gradAscent(dataMatIn, classLabels):
-    dataMatrix = mat(dataMatIn)
-    labelMat = mat(classLabels).transpose()
-    m, n = shape(dataMatrix)
-    alpha = 0.001
-    maxCycles = 500
-    weights = ones((n, 1))
-    for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)
-        error = (labelMat - h)
-        weights += alpha * dataMatrix.transpose() * error
-    return weights
-
-
-dataArr, loadMat = loadDataSet()
-weight = gradAscent(dataArr, loadMat)
